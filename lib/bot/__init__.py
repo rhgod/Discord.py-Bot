7e1b5d42f3a6790478b43464166f93507b3be8ca
Evrim Arda Kalafat <evrmarda@gmail.com>
2021-05-06 17:33:28 +0300

    0.0.6b
    +Basic hello command

--- lib/bot/__init__.py
+++ lib/bot/__init__.py
@@ -126,6 +126,7 @@ class Bot(BotBase):
 
 
     async def on_message(self, message):
-        pass
+        if not message.author.bot:
+            await self.process_commands(message)
 
 bot = Bot()
--- lib/cogs/fun.py
+++ lib/cogs/fun.py
@@ -1,9 +1,15 @@
+from random import choice
 from discord.ext.commands import Cog
+from discord.ext.commands import command
 
 class Fun(Cog):
     def __init__(self, bot):
         self.bot = bot
     
+    @command(name="hello", aliases=["hi", "hey"], hidden=True)  #+hello +hi +hey
+    async def say_hello(self, ctx):
+        await ctx.send(f"{choice(('Hello','Hey','Hi'))} {ctx.author.mention}!")
+
     @Cog.listener()
     async def on_ready(self):
         if not self.bot.ready:
